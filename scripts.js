3f92579950a46c13f0cd177b04cedda8f7ec1cd6
bekkopapa <kiyokuro1@gmail.com>
2023-04-12 02:55:27 +0900

    re_1

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// scripts.js 파일
+document.querySelector('.play-button').addEventListener('click', function () {
+    const blackOverlay = document.getElementById('blackOverlay');
+    blackOverlay.style.visibility = 'visible';
+    blackOverlay.style.opacity = 1;
+  
+    const nextPageUrl = 'interactive/i_1.html';
+  
+    // 암전이 끝난 후 페이지 이동을 실행합니다.
+    setTimeout(function () {
+      window.location.href = nextPageUrl;
+    }, 1000); // 여기에서 1000ms는 1초를 의미하며, 암전 시간과 동일한 시간을 사용해야 합니다.
+  });
+  
