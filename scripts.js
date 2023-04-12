0fc557820cd0b9e20806f78413fc4727ce013b8a
bekkopapa <kiyokuro1@gmail.com>
2023-04-12 23:24:59 +0900

    play

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
-// scripts.js 파일
-document.querySelector('.play-button').addEventListener('click', function () {
-    const blackOverlay = document.getElementById('blackOverlay');
+document.querySelector('.play-button').addEventListener('click', function() {
+  const blackOverlay = document.getElementById('blackOverlay');
+  const playButton = document.querySelector('.play-button');
+
+  // 이미지 변경
+  playButton.querySelector('img').src = 'icons/button_2.png';
+
+  // 오버레이 작동
+  blackOverlay.style.display = 'block';
+  setTimeout(function() {
     blackOverlay.style.visibility = 'visible';
     blackOverlay.style.opacity = 1;
-  
-    const nextPageUrl = 'Interactive/I_1.html';
-  
-    // 암전이 끝난 후 페이지 이동을 실행합니다.
-    setTimeout(function () {
-      window.location.href = nextPageUrl;
-    }, 1000); // 여기에서 1000ms는 1초를 의미하며, 암전 시간과 동일한 시간을 사용해야 합니다.
-  });
-  
+  }, 10);
+
+  // 페이지 이동
+  const nextPageUrl = 'Interactive/I_1.html';
+  setTimeout(function() {
+    window.location.href = nextPageUrl;
+  }, 1000);
+});
