41cfa155020d30b37291c97fb7cf72a0c4313bb7
bekkopapa <kiyokuro1@gmail.com>
2023-04-26 02:13:16 +0900

    qoute

--- scripts.js
+++ scripts.js
@@ -56,3 +56,39 @@ $('#SOHYUNSOO').on('change', function(event) {
         tableContainer.html('');
     }
     });
+
+            // Quotes array
+            const quotes = [
+                { text: '"오시리스들이야말로 개척군의 영웅이야. 탈영병 신세가 돼버린 건 참 안타까운 일일세. 우리가 몹쓸 짓을 한 거야. 몹쓸 짓을........"', author: '-차원우주개척군 사령관 막심 하이예크 / 프린테라' },
+                { text: '"내 인생의 1막은 씁쓸한 이혼으로 막을 내렸고, 2막은 전쟁의 참화 속에 있었다. 그리고 이제 내 생의 마지막이 될 3막이 펼쳐지려 하고 있다."', author: '-진 오시리스 / 프린테라' },
+                { text: '발아래 부서지는 모래언덕의 울부짖음이 가슴에 와 닿는구나.... 모로나두, 모로나두, 백색의 종양, 야후들의 공세는 흩날리는 모래폭풍만큼 지독하다오....', author: '-야후 / 프린테라' },
+                { text: '"영원히. 당신 남편은 거기 갇혀서 그렇게 고통받는 거죠. 무간지옥, 아비. 우리는 그곳을 그렇게 불러요."', author: '-호희 / 아비' },
+                { text: '"네가…… 네놈이 무슨 짓을…… 한 건지 똑똑히 봐 둬."', author: '-보영 / 아비' },
+                { text: '"안드로이드와 인간이길 포기한 더미 쓰는 놈들. 둘 사이에 경계를 없애주려는 거죠. 제가 밖에 있었으면 안드로이드에게 자유의지를 주고 진짜 해방을 선사했을 텐데, 아쉽네요."', author: '-소년 / 사건분석관K:미래범죄 수사일지' },
+                { text: '나는 늘 불안하고 목에 가시라도 걸린 듯 답답했다. 몇 번인가 심장이 두근거리며 예의 검은 얼룩이 희미하게 나타나기도 했다. 어쩌면 이것이 그 소년이 말했던 사건분석관의 결함인지도.', author: '-K / 사건분석관K:미래범죄 수사일지' },
+                { text: '"이 소돔의 악인들도 사실 서로 의지하며 살아가고 있는 셈이지. 강자는 더 강한 자에게 또 강자는 약자에게 그리고 약자는 그보다 더 약한 자에게 뺏고 빼앗기면서 말이다."', author: '-마테오 / 에덴' },
+                { text: '"썩어버린 새까맣고 더러운 물의 수면이 잔잔하다고 해서! 그 더러운 물에 파문이 이는 것이 두려워 깨끗한 물을 섞지 않는 게 옳은가요? 그게 헛된 일일까요?"', author: '-제이 / 에덴' },
+                { text: '"시간의 역설이 저를 쫓아내고 뭐고 간에 저 같은 노인은 그녀에게 다가갈 수조차 없었죠."', author: '-노인 / 시공간의 이방인' },
+                // Add more quotes as needed
+            ];
+    
+            function getRandomQuote() {
+                const randomIndex = Math.floor(Math.random() * quotes.length);
+                const quote = quotes[randomIndex];
+                return quote;
+            }
+    
+            function updateQuote() {
+                const quote = getRandomQuote();
+                $('blockquote small').text(quote.text);
+                $('footer small').text(quote.author);
+            }
+    
+            $(document).ready(function() {
+                // Update quote when the page is loaded
+                updateQuote();
+    
+                // Update quote when the dropdown menu selection changes
+                $('#SOHYUNSOO').on('change', updateQuote);
+            });
+ 
